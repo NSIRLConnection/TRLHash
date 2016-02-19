0b97f195e19ef65d0c47ad66ed6d76f0a9fbfc0a
Michael Yau <michael.yau@otgmanagement.com>
2016-02-19 18:23:21 -0500

    Initial Commit

---
+++ TRLHash/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TRLHash
+//
+//  Created by Michael Yau on 2/19/16.
+//  Copyright © 2016 NSIRLConnection. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TRLHash/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TRLHash
+//
+//  Created by Michael Yau on 2/19/16.
+//  Copyright © 2016 NSIRLConnection. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
